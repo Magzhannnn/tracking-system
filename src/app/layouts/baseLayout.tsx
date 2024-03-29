2f6cd8a3fc8b2fa2906f25b974022fe04ecf8b37
Turdygazin Magzhan <magzhan.turdygazin@gmail.com>
2024-03-29 15:16:05 +0500

    feature: added navbar

--- src/app/layouts/baseLayout.tsx
+++ src/app/layouts/baseLayout.tsx
@@ -1,11 +1,15 @@
 import { Header } from "@/widgets/header";
 import { PopUpProvider } from "../providers/PopUpContext";
+import { Navbar } from "@/widgets/navbar";
 
 function BaseLayout() {
   return (
     <>
       <PopUpProvider>
         <Header />
+        <div className="flex">
+          <Navbar />
+        </div>
       </PopUpProvider>
     </>
   );
